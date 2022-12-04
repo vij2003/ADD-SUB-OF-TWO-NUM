0a9ecde652053fa503c83735fafda2a47d8e1074
vij2003 <117030617+vij2003@users.noreply.github.com>
2022-12-04 13:34:42 +0530

    Create Adding.c

---
+++ Adding.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+    int x,y;
+    printf("Addition and subtraction of two numbers\n");
+    scanf("%d",&x);
+    scanf("%d",&y);
+    printf("Addition of %d and %d is %d\n",x,y,x+y);
+    printf("subtraction of %d and %d is %d",x,y,x-y);
+    
+    
+}
